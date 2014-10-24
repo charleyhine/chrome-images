0dc4a36d1c80eea025ef66080c9ee8318d828677
Charley Hine <charles.hine@gmail.com>
2014-10-23 23:07:57 -0700

    FOrmatting

--- cs/domScript.js
+++ cs/domScript.js
@@ -17,14 +17,12 @@ var DOMScript = {
         break;
       case "get":
         var list = [];
-				if (!window.location.origin)
-				     window.location.origin = window.location.protocol+"//"+window.location.host;
+				if (!window.location.origin){ window.location.origin = window.location.protocol+"//"+window.location.host; }
         $.each($(request.element), function(index, item) {
 					var src = $(item).attr("src")
 					var pat = /^https?:\/\//i;
-					if (!pat.test(src))
-							src = window.location.origin + '/' + src;
-          list.push(src);
+					if (!pat.test(src)){ src = window.location.origin + '/' + src; }
+					list.push(src);
         });
         sendResponse({result: list});
     }
